f3befe3b69a16b8d09a7d43d55d8fa72c0b40919
Sotnas <46525658+SantoGmz@users.noreply.github.com>
2024-01-15 00:01:29 -0400

    Primeros pasos, parte fundamental aprendiendo las teclas y parametros de python pyautogui, puedo usar "as" y convertirlo en variable

--- app3.py
+++ app3.py
@@ -1,12 +1,15 @@
 import pyautogui
+#time la libreria para poder tomar segundos por momentos
 import time
 
+#hotkey es para las teclas que usare 
 pyautogui.hotkey("win", "r")
 
 time.sleep(1)
+#write es para escribir lo que nesecite
 pyautogui.write('brave.exe "google.com"', interval=0.2)
 
 time.sleep(1)
 
-
+#press es como lo dice, presionar dichas teclas
 pyautogui.press("enter")
