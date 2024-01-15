d22495e29b9449f9c911d6567b34a34f89d16895
Sotnas <46525658+SantoGmz@users.noreply.github.com>
2024-01-15 00:52:06 -0400

    no entiendo como dibujar pero esta funcionando, mejor intento la cal

---
+++ app4.py
@@ -0,0 +1,40 @@
+import pyautogui
+import time
+
+
+
+pyautogui.hotkey("win", "r")
+
+time.sleep(1)
+
+pyautogui.write('mspaint.exe "', interval=0.2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+distance = 200
+while distance > 0:
+    pyautogui.drag(distance, 0, duration=0.5)   # move right
+    distance -= 5
+    pyautogui.drag(0, distance, duration=0.5)   # move down
+    pyautogui.drag(-distance, 0, duration=0.5)  # move left
+    distance -= 5
+    pyautogui.drag(0, -distance, duration=0.5)  # move u
