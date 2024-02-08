f58cbd670f54ade64d0e6a666f1887a4360b191f
Sotnas <46525658+SantoGmz@users.noreply.github.com>
2024-02-08 18:25:21 -0400

    generador de password hecho

--- generadorPassword.py
+++ generadorPassword.py
@@ -2,8 +2,18 @@ import string #para generar tanto letras texto y digitos
 import random #el nombre lo dice
 
 
-abcedario = string.ascii_letters
-numeros = string.digits
-simbbolosRaros = string.punctuation
+abcedario = string.ascii_letters #variable para almacenar letras
+numeros = string.digits #variable para almacenar Digitos numericos
+simbbolosRaros = string.punctuation #variable para almacenar los raritos como: !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
 test= string.capwords #No se que carajos es esto
-print(test)
+
+def generar_password(longitud): #esta funcion se encargar de crear la password
+    caracteres = abcedario + numeros +simbbolosRaros #sirve para almacenar todo en una variable aunque puedo llamarlo desde aqui y simplificar mas e codigo lo dejare asi para entender cuando yo buela a leer esto
+    password = "" #he tenido que creear la variable aqui porque no me dejaba crearla abajo
+    for i in range(longitud): #el bucle dara vueltas por todos lados hasta los numeros que halla proporcionado el usuario
+        password += random.choice(caracteres) #esta linea me dio demasiados problemas, aun no entiendo como lo resolvi pero lo importante es que funciona
+    print(password)  
+    
+    
+#hay que llamar la funcion y pasarle los valores          
+generar_password(8)    
