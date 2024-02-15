a1c7522e741ba8d0e9948df827789d6b456d21f5
Sotnas <46525658+SantoGmz@users.noreply.github.com>
2024-02-15 01:34:01 -0400

    tuve que empezar el codigo de nuevo porque tenia pocos comentarios y ya no entendia lo que estaba haciendo, mañana hay que dedicarle mas tiempo a los nombres de las variables para evitar confuciones

--- spam-ws_Whit_Tkinter/app.py
+++ spam-ws_Whit_Tkinter/app.py
@@ -1,31 +1,25 @@
-import keyboard #importing the keyboard
-import time #importing the mouse
-import pyautogui #importing the boot
-from tkinter import *
+import tkinter as tk
 
-ventana = Tk()
+ventana = tk.Tk()
 ventana.title("WhatsApp Spammer by GmzSan")
-ventana.geometry("200x250")
+ventana.geometry("150x200")
 ventana.resizable(False, False)#esto es para que no cambie el tamano nunca
+#funciones
+def on_click():
+    nuevo = entrada.get()
 
-
-def btn_click(msg):
-    mensaje = msg.get()
-    print(mensaje)
-    
-
-
-Label(ventana, text="Mensaje").grid(pady=0, padx=0, row=0, column=0)
-Label(ventana, text="veces").grid(row=2,column=0)
-
-
-msg = Entry(ventana, width=30).grid(padx=5, row=1, column=0)
-numOfmsg = Entry(ventana, width=3).grid(row=3, column=0)
-
-Button(ventana, text="iniciar", command=lambda: btn_click(msg), width=15).grid(row=4, column=0, pady=5)
+    print("Escribiste--> ", nuevo)
 
 
 
+entrada = tk.Entry(ventana)
+entrada.grid(column=0, row=1)
+entrada.pack()
+
+
+
+boton = tk.Button(text="ejecutar", command=on_click)
+boton.pack()
 
 
 
@@ -49,43 +43,6 @@ Button(ventana, text="iniciar", command=lambda: btn_click(msg), width=15).grid(r
 
 
 
-
-
-
-
-
-
-
-
-
-'''
-
-2
-2
-
-while keyboard.is_pressed('q') == False:
-   print("hola")
-   time.sleep(1)
-   
-print("DONE!")   
-
-
-i = 0
-
-msg = input("Escribe el mensaje que quieres enviar: --> ")
-numOfmsg = input("¿Cuantas veces sera enviado el mensaje?--> ")
-print(f'el mensaje sera enviado {numOfmsg} veces')
-
-
-time.sleep(3)
-while keyboard.is_pressed('q') == False:
-    
-    while i < int(numOfmsg):
-        pyautogui.typewrite(msg)
-        pyautogui.press('Enter')
-        time.sleep(0.3) 
-        i = i+1
-'''
 
 
 ventana.mainloop()        
