fe039ad167c035d0a0a3e40cc3487b1ababaad98
Sotnas <46525658+SantoGmz@users.noreply.github.com>
2024-02-17 17:20:58 -0400

    funciona, pero esta muy vacio, ahora quiero ponerle alguna imagen o algo para que no este tan vacio, para que quee un poco mejor o algo no lo se, pero el funcionamiento esta bien

--- spam-ws_Whit_Tkinter/app.py
+++ spam-ws_Whit_Tkinter/app.py
@@ -1,65 +1,71 @@
+'''
+    Tuve que remover el evento de que confirma si hay una letra presionada como por ejemplo la q para   
+    desactivar el programa.
+    
+'''
+
 import tkinter as tk
-import keyboard
 import pyautogui
 import time
+from tkinter import PhotoImage
 
 ventana = tk.Tk()
 ventana.title("WhatsApp Spammer by GmzSan")
-ventana.geometry("150x200")
-ventana.resizable(False, False)#esto es para que no cambie el tamano nunca
-#funciones
-numOfmsg = 2
+ventana.geometry("300x200")
+ventana.resizable(False, False)
 
+# Variable global para contar los mensajes enviados
+i = 0
+
+#la funcion
 def on_click():
-    nuevo = entrada.get()
-    if not nuevo: #if not sirve para comprobar si tiene algun vacio
-        print("no has escrito nada aun")
-        
+    global i
+    msg = entrada.get()
+    numOfmsg = entrada_num.get()  # Obtener el número de mensajes desde la entrada
+    i = 0  # Reiniciar la variable i
+    if not msg or not numOfmsg.isdigit(): # para confirmar si en algun lugar no hay digitos
+        print("No tiene numeros")
     else:
-        print("Escribiste--> ", nuevo)
+        print("Escribiste -->", msg)
+        print("Número de mensajes a enviar -->", numOfmsg)
         time.sleep(3)
-        while keyboard.is_pressed('q') == False:
-            i=0
-            while i < int(numOfmsg):
-                pyautogui.typewrite(nuevo)
-                pyautogui.press('Enter')
-                time.sleep(0.3) 
-                i = i+1
-                
+        while i < int(numOfmsg):
+            pyautogui.typewrite(msg)
+            pyautogui.press('Enter')
+            time.sleep(0.3)
+            i += 1
+            
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\empieza el codigo/////////////////////////////////////////
+try:
+    def cargar_imagen(ruta):
+        imagen = PhotoImage(file=ruta)
+        return imagen
+
+
+    # Cargar la imagen desde el archivo
+    ruta_imagen = "bart.png"  # Reemplaza con la ruta de tu imagen
+    imagen = cargar_imagen(ruta_imagen)
+
+    # Crear un Label para mostrar la imagen
+    label_imagen = tk.Label(ventana, image=imagen)
+    label_imagen.pack()
+except Exception as e:
+    print(f"---> {e}")
+
 
 
 
 entrada = tk.Entry(ventana)
-entrada.grid(column=0, row=1)
 entrada.pack()
 
 
+etiqueta_num = tk.Label(ventana, text="Número de mensajes a enviar:")
+etiqueta_num.pack()
 
-boton = tk.Button(text="ejecutar", command=on_click)
+entrada_num = tk.Entry(ventana)
+entrada_num.pack()
+
+boton = tk.Button(text="Ejecutar", command=on_click)
 boton.pack()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-ventana.mainloop()        
+ventana.mainloop()
