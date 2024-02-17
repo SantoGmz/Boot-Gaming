e574d0c7a5c538da9c9311cb27eca9834b82d2e3
Sotnas <46525658+SantoGmz@users.noreply.github.com>
2024-02-17 15:30:41 -0400

    Hasta ahora esta funcionando pero falla en lo de los numeros, el numero que le pongo no funciona para para, Tengo que paralo con la letra q

--- spam-ws_Whit_Tkinter/app.py
+++ spam-ws_Whit_Tkinter/app.py
@@ -1,14 +1,31 @@
 import tkinter as tk
+import keyboard
+import pyautogui
+import time
 
 ventana = tk.Tk()
 ventana.title("WhatsApp Spammer by GmzSan")
 ventana.geometry("150x200")
 ventana.resizable(False, False)#esto es para que no cambie el tamano nunca
 #funciones
+numOfmsg = 2
+
 def on_click():
     nuevo = entrada.get()
-
-    print("Escribiste--> ", nuevo)
+    if not nuevo: #if not sirve para comprobar si tiene algun vacio
+        print("no has escrito nada aun")
+        
+    else:
+        print("Escribiste--> ", nuevo)
+        time.sleep(3)
+        while keyboard.is_pressed('q') == False:
+            i=0
+            while i < int(numOfmsg):
+                pyautogui.typewrite(nuevo)
+                pyautogui.press('Enter')
+                time.sleep(0.3) 
+                i = i+1
+                
 
 
 
