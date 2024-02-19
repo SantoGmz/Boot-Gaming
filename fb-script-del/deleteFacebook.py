7c9e210d1fc204ecfc729aadc5dc0cc1bd8bbfcd
Sotnas <46525658+SantoGmz@users.noreply.github.com>
2024-02-18 20:27:30 -0400

    de pc

--- fb-script-del/deleteFacebook.py
+++ fb-script-del/deleteFacebook.py
@@ -2,9 +2,9 @@
 import pyautogui 
 import time
 
-time.sleep(2)
+time.sleep(0)
 
-
+'''
 try:
     busco = pyautogui.locateOnScreen('fb-btn-option.png')
     if busco is not None:
@@ -14,12 +14,63 @@ try:
 except  Exception as e:
     print(f"Error: {e}")
     
-    
-pyautogui.click('fb-btn-option.png')
-time.sleep(1)
-pyautogui.click('fb-btn-trash.png')
-time.sleep(1)
-pyautogui.click('fb-btn-move.png')
+'''
     
     
+try:
+    verificarBtnOpciones = pyautogui.locateOnScreen('fb-btn-option.png')    
+except Exception as e:
+    print(f"No se encontro el boton de opciones.")
     
+
+
+
+
+
+def borrardor():
+    
+    
+    pyautogui.click('fb-btn-option.png')
+    
+    verificarBtnTransh = pyautogui.locateOnScreen('fb-btn-trash.png')    
+
+        
+        #click al btn de basura\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    if verificarBtnTransh is not None:
+        pyautogui.click('fb-btn-trash.png')
+        print("Encontro el safacon")
+    else:
+        print("No Encontro el safacon")
+        time.sleep(2)
+
+        #############################Confirmar borrado
+
+        #click en el btn confirmar
+    pyautogui.waitFor("fb-btn-move.png", timeout=10)
+    verificarBtnMove = pyautogui.locateOnScreen('fb-btn-move.png')   
+    if verificarBtnMove is not None:
+        pyautogui.click('fb-btn-move.png')
+    else:
+        time.sleep(2)
+        pyautogui.click('fb-btn-move.png')
+#############################Confirmar borrado
+        
+        
+        
+
+
+if verificarBtnOpciones is not None:
+
+    borrardor()
+else:
+    print("No se encontro el boton opciones.")
+    time.sleep(2)
+    borrardor()
+    
+
+
+
+
+
+
+pyautogui.waitFor()
---
+++ fb-script-del/testDocumentation.py
@@ -0,0 +1,4 @@
+import pyautogui
+
+pyautogui.moveTo(1820, 50)   # moves mouse to X of 100, Y of 200.
+pyautogui.click()
